aa85339cb6af7c1ba2cb8c487090b1df01dfe9b0
Matthew Landauer <matthew@oaf.org.au>
2015-03-16 17:00:00 +1100

    A little debugging output

--- scraper.php
+++ scraper.php
@@ -1,6 +1,8 @@
 <?
 require 'scraperwiki.php';
 
+echo "Scraper running...";
+
 // Write out to the sqlite database using scraperwiki library
 scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 
