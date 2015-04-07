7ec0e00d8e5c70b6b91c3d9ac56172960d7ec6a0
Matthew Landauer <matthew@oaf.org.au>
2015-04-08 05:17:31 +1000

    Output loop to test streaming

--- scraper.php
+++ scraper.php
@@ -1,9 +1,13 @@
 <?
 require 'scraperwiki.php';
 
-echo "Scraper running...";
+echo "Scraper running...\n";
 
 // Write out to the sqlite database using scraperwiki library
 scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 
+for ($i=1; $i<=5; $i++) {
+  echo "$i...\n";
+  sleep(1);
+}
 ?>
